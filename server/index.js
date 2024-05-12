5fcf9953722b8a9f8748efbf3c5be45e21e7d7d1
bhanusunrise <pasindubhanukagood@gmail.com>
2024-05-12 12:04:31 +0530

    UI - Home, About, Prizing Created

--- server/index.js
+++ server/index.js
@@ -14,24 +14,26 @@ const db = mysql.createConnection({
   database: 'development_project_beta'
 });
 
+/*
 app.get('/', (req, res) => {
   return res.json("From backend side");
 });
+*/
 
 app.post('/login', (req, res) => {
-  const { username, password } = req.body; // Extract username and password from request
+  const { email, password } = req.body; // Extract username and password from request
 
   const sql = "SELECT * FROM users WHERE user_email = ? AND user_password = ?"; // Query to find user
-  db.query(sql, [username, password], (err, data) => {
+  db.query(sql, [email, password], (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Internal Server Error' });
+      return res.status(500).json({ message: 'Sorry. We are in a trouble.' });
     }
 
     if (data.length > 0) {
-      return res.status(200).json({ message: 'User is available' });
+      return res.status(200).json({ message: 'Valid User' });
     } else {
-      return res.status(404).json({ message: 'User is not available' });
+      return res.status(404).json({ message: 'Invalid User' });
     }
   });
 });
